437cdbff2491a8701648fd762e5b51d29b35b515
Heidi Gally <heidigally@gmail.com>
2019-11-07 14:06:53 -0600

    added array to model

--- lib/models/paranormal_experience.rb
+++ lib/models/paranormal_experience.rb
@@ -3,13 +3,15 @@ class Paranormal_Experience < ActiveRecord::Base
     belongs_to :haunt
     belongs_to :ghost_hunters
 
-    def self.visual_haunting
-       
+    def self.visual_haunting_
+        ["orbs", "shadow", "figure", "lights", "items moving", "apparition"]
     end
-
-    def self.auditory_haunting
+        
+    def self.auditory_haunting_array
+        ["disembodied", "voice", "scream", "growl", "footsteps", "knocking", "cries", "giggling", "laughing", "moaning", "unexplained sounds"]
     end
-
-    def self.physical_haunting
+        
+    def self.physical_haunting_array
+        ["thrown item", "cold spot", "poltergeist", "touch", "scratch", "possess"]
     end
 end
