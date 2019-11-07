7cf38ff761ff0fb81af86599d1a1eca524daad06
Heidi Gally <heidigally@gmail.com>
2019-11-07 12:54:15 -0600

    updated schema

--- db/migrate/20191107173756_paranormal_experiences.rb
+++ db/migrate/20191107173756_paranormal_experiences.rb
@@ -3,5 +3,6 @@ class ParanormalExperiences < ActiveRecord::Migration[5.0]
     create_table :paranormal_experiences do |t|
       t.integer :haunt_id
       t.integer :ghost_hunter_id
+    end
   end
 end
